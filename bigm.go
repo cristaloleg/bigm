6c1ff379dbca36f64b91ff09c006f5148b08a7ad
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-04-12 08:36:01 +0200

    init

---
+++ bigm.go
@@ -0,0 +1,82 @@
+package bigm
+
+import (
+	"math/big"
+)
+
+var (
+	bigZero   = big.NewInt(0)
+	bigOne    = big.NewInt(1)
+	Base      = []int32{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31}
+	BaseBig   = []*big.Int{}
+	Reverse   = [][]int32{}
+	BaseCount int
+)
+
+func init() {
+	Init()
+}
+
+// Init ...
+func Init() {
+	BaseCount = len(Base)
+
+	BaseBig = make([]*big.Int, BaseCount)
+	for i := range BaseBig {
+		BaseBig[i] = big.NewInt(int64(Base[i]))
+	}
+
+	Reverse = make([][]int32, BaseCount)
+	for i := range Reverse {
+		Reverse[i] = make([]int32, BaseCount)
+	}
+
+	for i := 0; i < BaseCount; i++ {
+		for j := i + 1; j < BaseCount; j++ {
+			Reverse[i][j] = modInverse(Base[i], Base[j])
+		}
+	}
+	println("Init done.")
+}
+
+func modInverse(a, m int32) int32 {
+	a = a % m
+	for x := int64(1); x < int64(m); x++ {
+		if (int64(a)*x)%int64(m) == 1 {
+			return int32(x)
+		}
+	}
+	return 0
+}
+
+// Int ...
+type Int struct {
+	nums []int32
+}
+
+// New ...
+func New() *Int {
+	ii := &Int{
+		nums: make([]int32, BaseCount),
+	}
+	return ii
+}
+
+// FromString ...
+func FromString(s string) *Int {
+	i, ok := big.NewInt(0).SetString(s, 10)
+	if !ok {
+		return nil
+	}
+	return FromBigInt(i)
+}
+
+// FromBigInt ...
+func FromBigInt(x *big.Int) *Int {
+	res := New()
+	for i := range Base {
+		tmp := big.NewInt(0).Rem(x, BaseBig[i])
+		res.nums[i] = int32(tmp.Int64())
+	}
+	return res
+}
